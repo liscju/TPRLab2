fc1be2cff650bd0aba0c81fc5f880f6c1b2af8d3
Bartłomiej Kumor <aargh1@gmail.com>
2015-03-22 16:37:46 +0100

    Launch config fixes

--- src/python/run-gandalf.sh
+++ src/python/run-gandalf.sh
@@ -1,1 +1,3 @@
 mpiexec -n 3 python python2.py -f ../gandalf-mpihosts
+mpiexec -n 5 python python2.py -f ../gandalf-mpihosts
+mpiexec -n 10 python python2.py -f ../gandalf-mpihosts
--- src/python/run.sh
+++ src/python/run.sh
@@ -1,1 +1,3 @@
 mpiexec -n 3 python python2.py
+mpiexec -n 5 python python2.py
+mpiexec -n 10 python python2.py
